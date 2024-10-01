b95b2e1e4259713cd5656caadc522498bc8ee59e
Cserich Philipp <phcaesar@t200-004.demo.tuwien.ac.at>
2024-10-01 14:00:57 +0200

    initial Doc Service

--- exec.sh
+++ exec.sh
@@ -9,5 +9,5 @@ docker run --rm --name=diplomathesis --workdir="" -it \
 
 # Inject CSS into each generated HTML file
 for html_file in $project_root/*.html; do
-  sed -i '1s/^/<style>\n/* Base Styles */\nbody {\n  color: #000000;\n}\n\n/* Footer Styles */\n@page {\n  @bottom-right {\n    content: "Lithoz X HTL - Spengergasse";\n  }\n  @bottom-left {\n    content: "Lithoz X HTL - Spengergasse";\n  }\n}\n\n/* Heading Styles */\nh1, h2, h3, h4, h5, h6 {\n  color: #000000;\n  font-size: 17px;\n  font-weight: bold;\n  line-height: 1.2;\n  margin-bottom: 10px;\n}\n\n/* Link Styles */\na {\n  color: #0000ff;\n}\n<\/style>\n/' "$html_file"
+  sed -i '1s/^/<style>\n/* Base Styles */\nbody {\n  color: #000000;\n}\n\n/* Footer Styles */\n@page {\n  @bottom-right {\n    content: "Socia";\n  }\n  @bottom-left {\n    content: "Socia";\n  }\n}\n\n/* Heading Styles */\nh1, h2, h3, h4, h5, h6 {\n  color: #000000;\n  font-size: 17px;\n  font-weight: bold;\n  line-height: 1.2;\n  margin-bottom: 10px;\n}\n\n/* Link Styles */\na {\n  color: #0000ff;\n}\n<\/style>\n/' "$html_file"
 done
