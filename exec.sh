d68fa03329d7cd82e4815dccc50009a8665df365
Cserich Philipp <phcaesar@MacBook-Pro-von-Cserich.local>
2024-10-01 22:31:48 +0200

    hightlighting

--- exec.sh
+++ exec.sh
@@ -8,6 +8,7 @@ docker run --rm --name=diplomathesis --workdir="" -it \
   -v $project_root/assets:/assets \
   -v $project_root/docs:/docs \
   asciidoctor/docker-asciidoctor \
-  find /documentation -name '*.adoc' -exec asciidoctor \
+  bash -c "gem install rouge && find /documentation -name '*.adoc' -exec asciidoctor \
+  -r rouge -a source-highlighter=rouge \
   -a stylesdir=assets -a stylesheet=theme.css -a linkcss=true -a outfilesuffix=.html \
-  -D /docs {} \;
+  -D /docs {} \;"
