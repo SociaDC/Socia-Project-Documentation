37c793979c3a84ade5afa5d75943c6a05a68c0f5
Cserich Philipp <phcaesar@t200-004.demo.tuwien.ac.at>
2024-10-01 10:52:29 +0200

    changing output dir

--- exec.sh
+++ exec.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
 project_root=$(pwd)
 
 docker run --rm --name=diplomathesis --workdir="" -it \
   -v $project_root/Documentation:/Documentation \
   -v $project_root/Assets:/Assets \
+  -v $project_root:/project_root \
   asciidoctor/docker-asciidoctor \
-  find /Documentation -name '*.adoc' -exec asciidoctor -a stylesdir=/Assets -a stylesheet=theme.css -a linkcss=true -a outfilesuffix=.html {} \;
+  find /Documentation -name '*.adoc' -exec asciidoctor \
+  -a stylesdir=/Assets -a stylesheet=theme.css -a linkcss=true -a outfilesuffix=.html \
+  -D /project_root {} \;
