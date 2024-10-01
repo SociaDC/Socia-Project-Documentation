dbf425a6093624a877bfdd816c2caabd3ec507ce
Cserich Philipp <phcaesar@MacBook-Pro-von-Cserich.local>
2024-10-01 15:48:43 +0200

    git naming conv for html data

--- exec.sh
+++ exec.sh
@@ -1,13 +1,13 @@
 project_root=$(pwd)
 
-# Create the output directory if it doesn't exist
-mkdir -p $project_root/output
+# Create the docs directory if it doesn't exist
+mkdir -p $project_root/docs
 
 docker run --rm --name=diplomathesis --workdir="" -it \
   -v $project_root/documentation:/documentation \
   -v $project_root/assets:/assets \
-  -v $project_root/output:/output \
+  -v $project_root/docs:/docs \
   asciidoctor/docker-asciidoctor \
   find /documentation -name '*.adoc' -exec asciidoctor \
   -a stylesdir=/assets -a stylesheet=/assets/theme.css -a linkcss=true -a outfilesuffix=.html \
-  -D /output {} \;
+  -D /docs {} \;
