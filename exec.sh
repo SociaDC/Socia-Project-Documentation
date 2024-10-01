ac9f7fbaacfc384175bdbb2ac1114d8859402d2c
Cserich Philipp <phcaesar@t200-004.demo.tuwien.ac.at>
2024-10-01 10:14:39 +0200

    Development purpose - HTML processed

--- exec.sh
+++ exec.sh
@@ -2,4 +2,8 @@
 
 project_root=$(pwd)
 
-docker run --rm --name=diplomathesis --workdir="" -it -v $project_root/Documentation:/Documentation -v $project_root/Assets:/Assets asciidoctor/docker-asciidoctor asciidoctor-pdf --theme /Assets/theme.yml -a pdf-fontsdir=/Assets/Fonts /Documentation/index.adoc
+docker run --rm --name=diplomathesis --workdir="" -it \
+  -v $project_root/Documentation:/Documentation \
+  -v $project_root/Assets:/Assets \
+  asciidoctor/docker-asciidoctor \
+  find /Documentation -name '*.adoc' -exec asciidoctor -a stylesdir=/Assets -a stylesheet=theme.css -a linkcss=true -a outfilesuffix=.html {} \;
